67dc4d6c56c01b5333c1053197cb0f8683d3a17b
bitsandtea <gashi.dite@gmail.com>
2023-05-12 18:05:32 +0100

    mock token

---
+++ contracts/MockToken.sol
@@ -0,0 +1,9 @@
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract MyToken is ERC20 {
+    constructor() ERC20("My Token", "MTK") {
+        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+    }
+}
+
